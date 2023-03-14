7c5e427c1a82b5f344bac87fe0a56c8be64040aa
Kadri Kajaste <kadrika@proeksert.ee>
2023-03-14 13:20:57 +0200

    FE improvements

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -6,13 +6,30 @@ import { SomePage } from './pages/somePage';
 import CitiesList from './pages/CitiesList';
 import CityDetail from './pages/CityDetail';
 
+import ErrorPage from './pages/ErrorPage';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <CitiesList />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: '/detail/:cityId',
+        element: <CityDetail />,
+    },
+]);
+
 function App() {
     return (
         <div className="App">
-            <Routes>
+            <RouterProvider router={router} />
+            {/*             <Routes>
                 <Route path="/" element={<CitiesList />} />
                 <Route path="/detail/:cityId" element={<CityDetail />} />
-            </Routes>
+            </Routes> */}
         </div>
     );
 }
